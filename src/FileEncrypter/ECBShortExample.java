0f4570a2385102e6725688a3f81878129a499cba
SteffenGiessing <Steffen.S.Giessing@gmail.com>
2022-02-24 14:43:22 +0100

    fixed library

--- src/FileEncrypter/ECBShortExample.java
+++ src/FileEncrypter/ECBShortExample.java
@@ -1,26 +1,30 @@
 package FileEncrypter;
+import ReadWrite.Write;
+import ReadWrite.Read;
 import org.bouncycastle.util.encoders.Hex;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
-
+import java.security.Security;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 public class ECBShortExample {
-        public static void main(String[] args) {}
-            String dir = "/Users/nielsj/Desktop/dev/gpg";
-            String plaintextFileName = dir + "/" + "MedicalRecordNielsJ.pdf";
-            byte[] keyBytes = Hex.decode("000102030405060708090a0b0c0d0e0f");
-            void encrypt () {
-                try {
-                    // reading
-                    byte[] input = library.FileUtil.readAllBytes(plaintextFileName);
-                    // encrypting
-                    Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding", "BC");
-                    SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
-                    cipher.init(Cipher.ENCRYPT_MODE, key);
-                    byte[] output = cipher.doFinal(input);
-                    // writing
-                    library.FileUtil.write("AES/ECB/PKCS5Padding", plaintextFileName, output);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+    public static void main(String[] args) {
+
+        String dir = "/Users/nielsj/Desktop/dev/gpg";
+        String plaintextFileName = dir + "/" + "MedicalRecordNielsJ.pdf";
+        byte[] keyBytes = Hex.decode("000102030405060708090a0b0c0d0e0f");
+
+        try {
+            // reading
+            byte[] input = Read.readAllBytes(plaintextFileName);
+            // encrypting
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding", "BC");
+            SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] output = cipher.doFinal(input);
+            // writing
+            Write.write("AES/ECB/PKCS5Padding", plaintextFileName, output);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+}
